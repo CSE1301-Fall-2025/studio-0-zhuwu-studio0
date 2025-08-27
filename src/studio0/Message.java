0b8986bdb88a25164ec6d6cc0fe2e33270d19d0c
ellazhu6 <e.r.zhu@wustl.edu>
2025-08-27 15:21:40 -0500

    hey

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,7 +3,7 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
-		System.out.println(" thefk   l;k j workshop! ");
+		System.out.println(" thhello kshop! ");
 		System.out.println("hellodafjisdjfkasdjflkjadsklf");
 	}   
 }
